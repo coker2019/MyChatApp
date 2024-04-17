bdbeace7b58746713c99eddc8d48fdf1bb1ae335
coker2019 <olabisi2k11@gmail.com>
2024-04-17 10:40:00 +0100

    Add component.

---
+++ chat-app/src/components/NewRoomForm.jsx
@@ -0,0 +1,15 @@
+// NewRoomForm.jsx
+
+import React from 'react';
+
+function NewRoomForm() {
+  // Component logic here
+  return (
+    <div>
+      {/* JSX content */}
+      <h2>New Room Form</h2>
+    </div>
+  );
+}
+
+export default NewRoomForm; // Export the component as default
