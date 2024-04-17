6de9ca6b0820c9c36dcdb2a220539f9d83f9b7d2
coker2019 <olabisi2k11@gmail.com>
2024-04-17 10:40:15 +0100

    Add component.

---
+++ chat-app/src/components/RoomList.jsx
@@ -0,0 +1,15 @@
+// RoomList.jsx
+
+import React from 'react';
+
+function RoomList() {
+  // Component logic here
+  return (
+    <div>
+      {/* JSX content */}
+      <h2>Room List</h2>
+    </div>
+  );
+}
+
+export default RoomList; // Export the component as default
